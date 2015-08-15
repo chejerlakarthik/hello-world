53e9cf42eb823b507560f268d71ebec32645fc64
Chejerla Karthik <chejerlakarthik@gmail.com>
2015-08-15 13:51:08 +0530

    Display developer name input from command-line

--- My-Hello-World/src/org/javabrains/HelloWorld.java
+++ My-Hello-World/src/org/javabrains/HelloWorld.java
@@ -15,7 +15,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World.");
-
+		
+		if(args.length !=1)
+		{
+			System.err.println("Need one argument from command-line");
+		}
+		else
+		{
+			System.out.println("The developer name is " + args[0]);
+		}
 	}
 
 }
